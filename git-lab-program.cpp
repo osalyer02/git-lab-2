a6b415ff0b4a59d5c6fffd93d52bb2dae03c73a9
Owen Salyer <os123918@ohio.edu>
2020-09-01 17:05:02 -0400

    Added program

---
+++ git-lab-program.cpp
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Owen Salyer
+ *        Date: 09/01/2020
+ * Description: Hello Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
